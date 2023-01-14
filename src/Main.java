1fcb882490004a037ba8774d38c049e6b6471ce6
Alex_888 <CAWA-888@mail.ru>
2023-01-14 18:18:55 +0300

    File new

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        // write your code here
+        Car newCar = new Car();
+        newCar.build();
+        newCar.buy();
+
+    }
+}
